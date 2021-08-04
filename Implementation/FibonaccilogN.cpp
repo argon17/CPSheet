ba01f82fa973edd6acaa63453f4bc5636fa8a4d4
argon <premsagark.ug18.ee@nitp.ac.in>
2021-08-04 11:40:36 +0530

    fibonacci in logN

--- Implementation/FibonaccilogN.cpp
+++ Implementation/FibonaccilogN.cpp
@@ -1,22 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
+
 const int hell = 1000000007;
 
-void multiply(long m1[2][2], long m2[2][2]) {
-    long a = (m1[0][0] * m2[0][0]) % hell + (m1[0][1] * m2[1][0]) % hell;
-    long b = (m1[0][0] * m2[0][1]) % hell + (m1[0][1] * m2[1][1]) % hell;
-    long c = (m1[1][0] * m2[0][0]) % hell + (m1[1][1] * m2[1][0]) % hell;
-    long d = (m1[1][0] * m2[0][1]) % hell + (m1[1][1] * m2[1][1]) % hell;
-    m1[0][0] = a % hell, m1[0][1] = b % hell;
-    m1[1][0] = c % hell, m1[1][1] = d % hell;
+void multiply(long m1[2][2], long m2[2][2])
+{
+    long a = ((m1[0][0] * m2[0][0]) % hell + (m1[0][1] * m2[1][0]) % hell) % hell;
+    long b = ((m1[0][0] * m2[0][1]) % hell + (m1[0][1] * m2[1][1]) % hell) % hell;
+    long c = ((m1[1][0] * m2[0][0]) % hell + (m1[1][1] * m2[1][0]) % hell) % hell;
+    long d = ((m1[1][0] * m2[0][1]) % hell + (m1[1][1] * m2[1][1]) % hell) % hell;
+    m1[0][0] = a, m1[0][1] = b;
+    m1[1][0] = c, m1[1][1] = d;
 }
 
 void powmatn(long mat[2][2], long n)
 {
     if (n < 2) return;
     long ans[2][2] = {1, 0, 0, 1};
-    while (n) {
+    while (n)
+    {
         if (n & 1) multiply(ans, mat), --n;
         else multiply(mat, mat), n >>= 1;
     }
@@ -24,8 +26,9 @@ void powmatn(long mat[2][2], long n)
     mat[1][0] = ans[1][0], mat[1][1] = ans[1][1];
 }
 
-int solve(int n) {
-    long mat[2][2] = {1, 1, 1 , 0};
+int solve(int n)
+{
+    long mat[2][2] = {1, 1, 1, 0};
     powmatn(mat, n - 1);
     return mat[0][0];
 }
