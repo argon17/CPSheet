a0cd97f28a0f663cc7ae310cb734c8b8b18e62cf
argon <premsagark.ug18.ee@nitp.ac.in>
2021-08-29 09:36:25 +0530

    cool implementation of lower bound

--- Binary Search/LowerBound.cpp
+++ Binary Search/LowerBound.cpp
@@ -1,14 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-int searchInsert(vector<int>& nums, int target) {
-
-    int lo = 0, hi = nums.size()-1;
-
-    while (lo <= hi) {
-        int mid = (hi+lo)>>1;
-        // condition for neglecting left half
-        if (nums[mid] < target) lo = mid+1;
-        else hi = mid-1;
+int lower_bound(vector<int> &nums, int target){
+    int l = 0, r = nums.size();
+    while(l < r){
+        int mid = (l + r) >> 1;
+        if(nums[mid] < target) l = mid + 1;
+        else r = mid;
     }
-    return lo;
+    return l;
 }
